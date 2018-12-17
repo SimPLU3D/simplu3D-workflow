6c04a9f7d6d4bea6fd575837fc0b69e86c09aed2
Mickaël Brasebin <mickael.brasebin@ign.fr>
2018-12-17 11:33:24 +0100

    Updating script and .gitignore

--- get_scripts.sh
+++ get_scripts.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-wget https://github.com/julienperret/aggregator/releases/download/v1.0/aggregator-1.0.zip
+## Version of aggregator script
+aggregatorscriptversion='1.0'
 
-unzip aggregator-1.0.zip
+## Version of SimPLU3D-openmole release
+simplu3dopenmolescripversion='1.0'
+
+
+wget https://github.com/julienperret/aggregator/releases/download/v$aggregatorscriptversion/aggregator-$aggregatorscriptversion.zip
+
+unzip aggregator-$aggregatorscriptversion.zip
+
+rm aggregator-$aggregatorscriptversion.zip
 
 echo "You can now run the aggregator script in the directory aggregator-1.0/bin/"
 
 echo "For instance ./aggregator-1.0/bin/aggregator inputData cadastre truth temp output_workflow/parcels.shp"
+
+
+wget https://github.com/SimPLU3D/simplu3D-openmole/releases/download/V$simplu3dopenmolescripversion/SimPLU3D-openmole-release.zip
+
+unzip SimPLU3D-openmole-release.zip
+
+rm SimPLU3D-openmole-release.zip
